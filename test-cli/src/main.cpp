035992fb0a19e8f299f900a88a54d58d8c12f2ec
mc18g13 <matthewcaswell@hotmail.co.uk>
2020-11-22 12:12:59 +0000

    update test cli

--- test-cli/src/main.cpp
+++ test-cli/src/main.cpp
@@ -7,19 +7,15 @@
 
 MainMenuHandler mainMenu;
 
-#define MOTOR_NOISE_TEST_INDEX 1
-#define FILTER_TEST_INDEX      2
-#define MARG_TEST_INDEX        3
-
 MotorNoiseTest motorNoiseTest;
 FilterTest filterTest;
 MARGTest margTest;
 
 void setup() {
 
-  mainMenu.addOptionHandler(MOTOR_NOISE_TEST_INDEX, &motorNoiseTest);
-  mainMenu.addOptionHandler(FILTER_TEST_INDEX, &filterTest);
-  mainMenu.addOptionHandler(MARG_TEST_INDEX, &margTest);
+  mainMenu.addOptionHandler(&motorNoiseTest);
+  mainMenu.addOptionHandler(&filterTest);
+  // mainMenu.addOptionHandler(&margTest);
   mainMenu.setup();
 
 }
